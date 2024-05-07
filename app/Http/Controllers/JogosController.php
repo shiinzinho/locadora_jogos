b97c3a5c0bb1b80969fe032644632248198ecd7a
shiinzinho <gabrieldosantoslacerda@gmail.com>
2024-05-07 14:52:17 -0300

    first commit

---
+++ app/Http/Controllers/JogosController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jogos;
+use Illuminate\Http\Request;
+
+class JogosController extends Controller
+{
+    public function jogos(Request $request)
+    {
+        $jogos = Jogos::create([
+            'nome' => $request-> nome,
+            'preco'=> $request-> preco,
+            'descricao'=> $request-> descricao,
+            'classificacao'=> $request-> classificacao,
+            'plataformas'=> $request-> plataformas,
+            'desenvolvedor'=> $request-> desenvolvedor,
+            'distribuidora'=> $request-> distribuidora,
+
+        ]);
+        return response()->json([
+            "sucess" => true,
+            "message" => "Registro de jogo bem-sucedido",
+            "data" => $jogos
+        ]);
+    }
+}
